38c90cdcbe964cd60ebfe70201eebab905db106f
ToBinio <tobias.frischmann1@gmail.com>
2023-06-18 19:01:11 +0200

    frontend login with github

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,21 +1,76 @@
-import {redirect} from "solid-start";
+import {createRouteData, parseCookie, redirect, refetchRouteData, useRouteData, useServerContext} from "solid-start";
+import {isServer, Show} from "solid-js/web";
+import {User} from "~/types/types";
 
 export default function Home() {
 
-  function signIn() {
-    let clientId = "b06e85e5bb8f2bced706";
-    let clientSecret = "4afc3de105b9b0cc76168909c6e7238f126fca09";
+    let data = useRouteData<typeof routeData>();
 
-    window.location.href = "https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=read:user" ;
-  }
+    function signIn() {
+        let clientId = "b06e85e5bb8f2bced706";
 
-  return (
-    <main>
-      <h2>Hello</h2>
+        window.location.href = "https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=read:user";
+    }
 
-      <button onclick={signIn}>
-        sign in
-      </button>
-    </main>
-  );
+    function logOut() {
+        document.cookie = "githubToken=;max-age=0"
+        refetchRouteData("cookies").then(
+            () => {
+                refetchRouteData("user").then()
+            }
+        )
+    }
+
+    return (
+        <main>
+            <h2>Hello
+                <Show when={data.user() != undefined}>
+                    , {data.user()?.login}
+                    <img src={data.user()?.avatar_url} alt="avatar"/>
+                </Show>
+            </h2>
+
+            <button onclick={signIn}>
+                sign in
+            </button>
+
+            <Show when={data.cookies()?.githubToken != undefined}>
+                <button onclick={logOut}>
+                    log out
+                </button>
+            </Show>
+        </main>
+    );
+}
+
+export function routeData() {
+    let cookies = createRouteData(() => {
+        const event = useServerContext();
+
+        let data = isServer
+            ? event.request.headers.get("cookie") ?? ""
+            : document.cookie;
+
+        console.log("update cookies " + data);
+
+        return parseCookie(data) as { githubToken: string };
+    }, {key: "cookies"})
+
+    let user = createRouteData(async ([, cookies]) => {
+
+        if (cookies == undefined || typeof cookies == 'string') throw "cookies could not be loaded"
+
+        if (cookies.githubToken == undefined) return undefined
+
+        let res = await fetch("https://api.github.com/user", {
+            method: "GET",
+            headers: {Authorization: "Bearer " + cookies.githubToken}
+        })
+
+        console.log("update user data: " + cookies.githubToken);
+
+        return (await res.json()) as User;
+    }, {key: () => ["user", cookies()]});
+
+    return {cookies, user}
 }
