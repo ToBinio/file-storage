fee6a6d31966f0be6eb19cacd5b703d61ff60685
to_binio <tobias.frischmann1@gmail.com>
2023-06-19 15:20:45 +0200

    add style

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,64 +1,56 @@
-import {
-    createCookieSessionStorage,
-    createRouteData, parseCookie, redirect,
-    Title,
-    useParams,
-    useRouteData,
-    useSearchParams,
-    useServerContext
-} from "solid-start";
+import {useRouteData, useSearchParams} from "solid-start";
 import {useNavigate} from "@solidjs/router";
 import {isServer} from "solid-js/web";
 import {createServerData$} from "solid-start/server";
 
 export default function Home() {
-    const {token} = useRouteData<typeof routeData>();
-    const navigate = useNavigate();
+  const {token} = useRouteData<typeof routeData>();
+  const navigate = useNavigate();
 
-    let data = token();
+  let data = token();
 
-    if (!isServer) {
-        if (data == undefined) {
-            console.error(data)
-        } else if ('error' in data) {
-            console.error(data.error)
-        } else {
-            document.cookie = `githubToken=${data.access_token}; max-age=${60 * 60 * 24 * 7}`;
-        }
-
-        navigate("/")
+  if (!isServer) {
+    if (data == undefined) {
+      console.error(data)
+    } else if ('error' in data) {
+      console.error(data.error)
+    } else {
+      document.cookie = `githubToken=${data.access_token}; max-age=${60 * 60 * 24 * 7}`;
     }
 
-    return <>loading...</>;
+    navigate("/")
+  }
+
+  return <>loading...</>;
 }
 
 
 export function routeData() {
-    const token = createServerData$(async () => {
-        const params = useSearchParams<{ code: string }>()
+  const token = createServerData$(async () => {
+    const params = useSearchParams<{ code: string }>()
 
-        //todo use .env
+    //todo use .env
 
-        const clientId = "b06e85e5bb8f2bced706";
-        const clientSecret = "4afc3de105b9b0cc76168909c6e7238f126fca09";
+    const clientId = "b06e85e5bb8f2bced706";
+    const clientSecret = "4afc3de105b9b0cc76168909c6e7238f126fca09";
 
-        let res = await fetch(`https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${params[0].code}`, {
-            method: "POST",
-            headers: {accept: "application/json"}
-        })
-
-        return (await res.json()) as (Error | Token)
+    let res = await fetch(`https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${params[0].code}`, {
+      method: "POST",
+      headers: {accept: "application/json"}
     })
 
-    return {token};
+    return (await res.json()) as (Error | Token)
+  })
+
+  return {token};
 }
 
 type Error = {
-    error: string,
-    error_description: string,
-    error_uri: string
+  error: string,
+  error_description: string,
+  error_uri: string
 }
 
 type Token = {
-    access_token: string
+  access_token: string
 }
